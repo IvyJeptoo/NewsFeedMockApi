dccaa0e9c6977c2dc0fd94e31d52d5f666d153b4
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-07-25 03:55:55 +0300

    deployment

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // <== Will be created later
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; // <== You can change the port
+const port = process.env.PORT || 3000; 
 
 server.use(middlewares);
 server.use(router);
